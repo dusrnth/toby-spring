fba76c72d6705258a87350f3238ebf6ef7b5eea0
kyupid <iamscvv@gmail.com>
2023-03-26 13:30:21 +0900

    fix: UserDao 에 있는 ConnectionMaker 를 선택하는 책임을 제3자인 클라이언트(UserDaoTest)에게 넘기도록 수정

--- chapter01/src/main/java/springbook/user/UserDaoTest.java
+++ chapter01/src/main/java/springbook/user/UserDaoTest.java
@@ -1,13 +1,14 @@
 package springbook.user;
 
+import springbook.user.dao.DConnectionMaker;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
-public class App {
+public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
+        UserDao dao = new UserDao(new DConnectionMaker());
         dao.dbInit();
 
         User user = new User();
