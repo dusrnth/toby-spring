677111daa4d621decabc131f8d287f6671628a1c
kyupid <iamscvv@gmail.com>
2023-03-26 13:05:02 +0900

    impl: 인터페이스를 이용해 DB 커넥션 기능의 의존성을 느슨하게 하고 고객사마다 직접구현하여 사용할 수 있도록 구현

--- chapter01/src/main/java/springbook/user/dao/UserDao.java
+++ chapter01/src/main/java/springbook/user/dao/UserDao.java
@@ -6,14 +6,14 @@ import java.sql.*;
 
 public class UserDao {
 
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
 
     public UserDao() {
-        this.simpleConnectionMaker = new SimpleConnectionMaker();
+        connectionMaker = new DConnectionMaker();
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -27,7 +27,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -50,7 +50,7 @@ public class UserDao {
 //    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
     public void dbInit() throws SQLException, ClassNotFoundException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement(
                 "delete from users where id = ?"
         );
