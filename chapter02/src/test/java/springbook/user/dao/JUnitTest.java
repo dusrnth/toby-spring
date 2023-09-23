33e07c608837a310869f10734e136807ea399ee0
kyupid <iamscvv@gmail.com>
2023-09-23 16:06:27 +0900

    2.5.2 학습 테스트 예제 - 리스트 2-27 스프링 테스트 컨텍스트에 대한 학습 테스트

--- chapter02/src/test/java/springbook/user/dao/JUnitTest.java
+++ chapter02/src/test/java/springbook/user/dao/JUnitTest.java
@@ -1,30 +1,50 @@
 package springbook.user.dao;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 class JUnitTest {
+    @Autowired
+    ApplicationContext context; // 테스트 컨텍스트가 매번주입해주는 애플리케이션 컨텍스트는 항상 같은 오브젝트?
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     void test1() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertThat(contextObject).isIn(null, this.context);
+        contextObject = this.context;
     }
 
     @Test
     void test2() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertThat(contextObject).isIn(null, this.context);
+        contextObject = this.context;
     }
 
     @Test
     void test3() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertThat(contextObject).isIn(null, this.context);
+        contextObject = this.context;
     }
 }
